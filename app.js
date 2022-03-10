bf15092aff40dc4731d7c165b5cf87a4c2d64ce9
Taeyeon <aaronktaey@gmail.com>
2022-03-10 23:11:04 +0900

    Code Challenge 1

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const title = document.querySelector("#title");
-
-function handeTitleMouseOn() {
-  console.log("mouse on!");
+const body = document.querySelector("body");
+function handeTitleMouseEnter() {
+  title.innerText = "The mouse is here!";
+  title.style.color = "blue";
+}
+function handeTitleMouseLeave() {
+  title.innerText = "The mouse is gone!";
+  title.style.color = "red";
+}
+function handleDocRightClick() {
+  title.innerText = "That was a right click!";
+  title.style.color = "orange";
+}
+function handleBrwserResize() {
+  title.innerText = "You just resized!";
+  title.style.color = "purple";
 }
 
-title.addEventListener("mouseon", handeTitleMouseOn);
+title.addEventListener("mouseenter", handeTitleMouseEnter);
+title.addEventListener("mouseleave", handeTitleMouseLeave);
+document.addEventListener("contextmenu", handleDocRightClick);
+window.addEventListener("resize", handleBrwserResize);
