7fa37a23af32fc67dc2a67aa8717b38fa9b81859
Taeyeon <aaronktaey@gmail.com>
2022-03-10 23:39:00 +0900

    Code Challenge 1

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
+// elements to handle
 const title = document.querySelector("#title");
 const body = document.querySelector("body");
-function handeTitleMouseEnter() {
-  title.innerText = "The mouse is here!";
-  title.style.color = "blue";
-}
-function handeTitleMouseLeave() {
-  title.innerText = "The mouse is gone!";
-  title.style.color = "red";
-}
-function handleDocRightClick() {
-  title.innerText = "That was a right click!";
-  title.style.color = "orange";
-}
-function handleBrwserResize() {
-  title.innerText = "You just resized!";
-  title.style.color = "purple";
-}
+const colors = ["blue", "red", "orange", "purple"];
 
-title.addEventListener("mouseenter", handeTitleMouseEnter);
-title.addEventListener("mouseleave", handeTitleMouseLeave);
-document.addEventListener("contextmenu", handleDocRightClick);
-window.addEventListener("resize", handleBrwserResize);
+// functions to use
+const superEventHandler = {
+  handeTitleMouseEnter: function () {
+    title.innerText = "The mouse is here!";
+    title.style.color = colors[0];
+  },
+  handeTitleMouseLeave: function () {
+    title.innerText = "The mouse is gone!";
+    title.style.color = colors[1];
+  },
+  handleDocRightClick: function () {
+    title.innerText = "That was a right click!";
+    title.style.color = colors[2];
+  },
+  handleBrwserResize: function () {
+    title.innerText = "You just resized!";
+    title.style.color = colors[3];
+  },
+};
+
+//events to handle
+title.addEventListener("mouseenter", superEventHandler.handeTitleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handeTitleMouseLeave);
+document.addEventListener("contextmenu", superEventHandler.handleDocRightClick);
+window.addEventListener("resize", superEventHandler.handleBrwserResize);
