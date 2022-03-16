23f5118c1d72a544b4c1819652acec0d86456e02
aaronktaey <aaronktaey@gmail.com>
2022-03-16 20:04:32 +0900

    Xmas-D-Day

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-const body = document.querySelector("body");
-const colors = ["red", "orange", "yellow", "green", "blue", "darkblue", "purple"]
-function handleWindowResize(){
-    const wInnerWidth = window.innerWidth;
-    if(wInnerWidth < 200){
-        body.style.backgroundColor = colors[0];
-    }else if(wInnerWidth < 400){
-        body.style.backgroundColor = colors[1];
-    }else if(wInnerWidth < 600){
-        body.style.backgroundColor = colors[2];
-    }else if(wInnerWidth < 800){
-        body.style.backgroundColor = colors[3];
-    }else if(wInnerWidth < 1000){
-        body.style.backgroundColor = colors[4];
-    }else if(wInnerWidth < 1200){
-        body.style.backgroundColor = colors[5];
-    }else{
-        body.style.backgroundColor = colors[6];
-    }
+'use strict';
+const title = document.querySelector("#title");
+const time = document.querySelector("#time");
+let xMas = new Date("2022-12-25T00:00:00");
+let timeLeft;
+const timePassBy = () =>{
+    const now = new Date();
+    timeLeft = xMas.getTime() - now.getTime();
+    
+    const day = Math.floor(timeLeft/(1000*60*60*24));
+    const hh = Math.floor(timeLeft%(1000*60*60*24)/(1000*60*60));
+    const mm = Math.floor(timeLeft%(1000*60*60)/(1000*60));
+    const ss = Math.floor(timeLeft%(1000*60)/1000);
+    
+    time.innerHTML = `${day}d ${hh>=10?hh:"0"+hh}h ${mm>=10?mm:"0"+mm}m ${ss>=10?ss:"0"+ss}s`;
 }
-
-window.addEventListener("resize", handleWindowResize);
+timePassBy();
+setInterval(() => timePassBy(), 1000);
