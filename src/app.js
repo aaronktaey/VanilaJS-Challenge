f772ac06d244de529c495f93655e51a2b13a2ed3
Taeyeon <aaronktaey@gmail.com>
2022-03-20 13:54:10 +0900

    background

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const focusDiv = document.querySelector("#focusDiv");
 const focus = document.querySelector("#focus");
 const focusInput = document.querySelector("#focusInput");
 const focusButton = document.querySelector("#focusButton");
-const bgImg = document.querySelector("#backgroundImage");
+const bgImg = document.querySelector("html");
 
 function updateClock() {
   const timeNow = new Date();
@@ -48,8 +48,9 @@ function drawBody() {
   ) {
     focusDiv.innerHTML = `<h3>${localStorageFocus}</h3>`;
   }
-
-  bgImg.src = `img/${randomNumberGenerator(7)}.jpg`;
+  document.querySelector(
+    "html"
+  ).style.backgroundImage = `url(img/${randomNumberGenerator(7)}.jpg)`;
 }
 
 function handleNameButtonClick() {
